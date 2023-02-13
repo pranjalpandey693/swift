d9674910e6126ecb0f432612a3eaddedb7f34a0a
Pranjal pandey <pranjalpandey693@gmail.com>
2023-02-14 02:45:47 +0530

    progress

--- swift tutorial/youtube/closures.playground/Contents.swift
+++ swift tutorial/youtube/closures.playground/Contents.swift
@@ -41,10 +41,58 @@ customAdd( 20,30) {
     $0 + $1
 }
 
+
+
 //passing some special closures
 let ages = [30,20,19,40]
 ages.sorted(by: <)
 ages.sorted(by: >)
+
+
+func customAdd2(
+    using function: (Int, Int ) -> Int,
+    _ lhs: Int,
+    _ rhs: Int
+  //closure
+) -> Int {
+    function(lhs,rhs)
+}
+
+
+customAdd2(
+    using: { $0 + $1 + 20},
+    20,
+    30
+    )
+
+func add10To(value: Int ) -> Int {
+    value + 10
+}
+
+func add20To(_ value: Int ) -> Int {
+    value + 20
+}
+
+func doAdditon(
+    on value: Int,
+    using function: (Int) -> Int
+) -> Int{
+function(value)
+}
+
+doAdditon(on: 20) { (value) in
+    
+    value + 20
+}
+
+doAdditon(on: 20,
+          using: add20To(_:)
+)
+doAdditon(on: 20,
+          using: add10To(value:)
+)
+
+
 
 
 
